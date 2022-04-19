acd09679bb01034790a494a0305a8ab02d6607c6
a7med-hamdy <hamdiahmed906@gmail.com>
2022-04-19 15:50:36 +0200

    numeric branches

--- back-end/server/src/main/java/com/prodcons/server/graph/Graph.java
+++ back-end/server/src/main/java/com/prodcons/server/graph/Graph.java
@@ -2,20 +2,13 @@ package com.prodcons.server.graph;
 
 
 
-
+import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.DefaultDirectedWeightedGraph;
 
 
 public class Graph {
-    // private DefaultDirectedWeightedGraph<Node, Edge> graph = new DefaultDirectedWeightedGraph<>(Edge.class);
+    private DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph = new DefaultDirectedWeightedGraph<>(DefaultWeightedEdge.class);
     public Graph (){
-        // Edge e = new Edge("s^2");
-        // Node n1 = new Node("n1");
-        // Node n2 = new Node("n2");
-        // this.graph.addVertex(n1);
-        // this.graph.addVertex(n2);
-        // this.graph.addEdge(n1, n2, e);
-        // System.out.println(this.graph.getEdgeSource(e).getName());
-        // System.out.println(this.graph.getEdgeWeight(e));
+        
     } 
 }
