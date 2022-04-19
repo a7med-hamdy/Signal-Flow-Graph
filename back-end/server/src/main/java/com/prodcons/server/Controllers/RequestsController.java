eec02aa2e689dd110d45d34c5fd7ed6d8d7a2a27
Ahmed-Ashour-2024 <mody.ashour.726@gmail.com>
2022-04-19 18:17:40 +0200

    requests updated (back & front)

--- back-end/server/src/main/java/com/prodcons/server/Controllers/RequestsController.java
+++ back-end/server/src/main/java/com/prodcons/server/Controllers/RequestsController.java
@@ -1,23 +1,35 @@
 package com.prodcons.server.Controllers;
+import java.util.regex.Pattern;
 
 import com.prodcons.server.graph.*;
-
-
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
 public class RequestsController {
-    Graph graph=new Graph();
-    careTaker cTaker= new careTaker();
+    Graph graph = new Graph();
 
-/**************************************************
- * Craeation board requests                       *
- **************************************************/
-    @PostMapping("/+Q")
-    public boolean addQueue(){
+    //using Regex to check if the weights of the edges are numbers or variables
+    /*the numbers grammar "-?\\d+(\\.\\d+)?":
+        "-?"         -> negative sign is optional
+        "\\d+"       -> one or more digits
+        "(\\.\\d+)?" -> fraction part is optional
+    */
+    private Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+    public boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false; 
+        }
+        return pattern.matcher(strNum).matches();
+    }
+    /**************************************************
+     * Craeation Graph requests                       *
+     **************************************************/
+    //adding a node request
+    @PostMapping("/+node")
+    public boolean add_node(){
         try{
-            this.graph.addQueue();
+            // this.graph.addNode();
         }
         catch(Exception e){
             e.printStackTrace();
@@ -26,28 +38,23 @@ public class RequestsController {
         return true;
     }
 
-    @PostMapping("/+M")
-    public boolean addMachine(){
-        try{
-            this.graph.addMachine();
-        }
-        catch(Exception e){
-            e.printStackTrace();
-            return false;
-        }
-        System.out.println("Machine");
-        return true;
-    }
-
-    //for example: "/+shm/M1/Q2" adding an arrow from (M1) to (Q2)
-    @PostMapping("/+shm/{from}/{to}")
-    public boolean addArrow(
+    //adding an edge request
+    //for example: "/+edge/x1/x2/40" adds an edge from (x1) to (x2) with weight 40
+    @PostMapping("/+edge/{from}/{to}/{weight}")
+    public boolean add_edge(
         @PathVariable("from") String from,
-        @PathVariable("to") String to)
+        @PathVariable("to") String to,
+        @PathVariable("weight") String weight)
+
     {
         String str = "";
         try{
-            str = graph.addEdge(from, to);
+            if(isNumeric(weight)){ //Numeric weights
+                // str = graph.addEdge(from, to, Double.valueOf(weight));
+            }
+            else{ //variable weights
+                // str = graph.addEdge(from, to, weight);
+            }
         }
         catch(Exception e){
             e.printStackTrace();
@@ -57,26 +64,15 @@ public class RequestsController {
         {
             return true;
         }
-        return false;
+        else
+            return false;
     }
 
-/**************************************************
- * Getting & Deleting requests                    *
- **************************************************/
-    @GetMapping("/getGraph")
-    public String getGraph(){
-        try{
-            return this.graph.getShapes();
-        }
-        catch(Exception e){
-            return "Graph";
-        }
-    }
- 
+    //clear the whole graph to draw another one
     @DeleteMapping("/clear")
     public boolean clearGraph(){
         try{
-            this.graph=new Graph();
+            this.graph = new Graph();
         }
         catch(Exception e){
             e.printStackTrace();
@@ -86,21 +82,22 @@ public class RequestsController {
     }
 
 
-/**************************************************
- * Simulation requests                            *
- **************************************************/
-    @PostMapping("/play")
-    public void play(){
-        this.graph.startSimulation();
-        this.origin.setState(graph);
-        this.cTaker.addMemento(origin.getMemento());
-    }
-
+    /**************************************************
+     * Solution requests                              *
+     **************************************************/
+    //checks if it's a valid Signal-Flow-Graph or not 
     @PostMapping("/validate")
     public boolean validate(){
-        return this.graph.valdiateSimulation();
+        return /* this.graph.valdiateGraph() */ true;
     }
-    @PostMapping("/save")
+    
+    //solves & stores the solution
+    @PostMapping("/solve")
+    public void solve(){
+        // this.graph.solve();
+    }
+
+    /* @PostMapping("/save")
     public void save(@RequestParam("shape") String shapes)
     {
         this.graph.setShapes(shapes);
@@ -126,5 +123,5 @@ public class RequestsController {
     @PostMapping("/replay")
     public void replay(){
         this.graph.replay();
-    }
+    } */
 }
